9010eae97ca42255b48ce1d3d63380c844d66806
suntm1001 <2320866489@qq.com>
2023-03-21 15:03:43 +0800

    git 测试

--- provider/src/main/java/com/stm/controller/HelloController.java
+++ provider/src/main/java/com/stm/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
-        
+        String str ="git test";
         return "l";
     }
 }
