f00fc4bd1882b360b0bf868b86f3d46a4379667b
suntm1001 <2320866489@qq.com>
2023-03-26 14:56:33 +0800

    dev-two新增代码

---
+++ provider/src/main/java/com/stm/controller/HelloControllerYes.java
@@ -0,0 +1,15 @@
+package com.stm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControllerYes {
+    @RequestMapping("/hello")
+    public String hello(){
+        String str ="git@github.com:suntm1001/nacos_project.git";
+        String s = "ss";
+        String hebing="da";
+        return "l";
+    }
+}
